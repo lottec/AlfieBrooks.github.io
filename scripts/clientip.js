fb73916b2ccdf8d95e8d0cfe470dc0f2a6df01d9
Rands <tra08@ostml0222993.bskyb.com>
2016-01-05 09:10:08 +0000

    first stuff

---
+++ scripts/clientip.js
@@ -0,0 +1,23 @@
+function getClientIP(fromInternal) {
+   var clientIP = getIpAddress();
+   ga('create', 'UA-67638506-1', 'auto');
+   //ga('create', 'UA-67638506-2', {'cookieDomain': 'none'});
+
+   if(clientIP.indexOf("90.216.134.") > -1 || clientIP.indexOf("90.216.150.") > -1) {
+     ga('set', 'dimension1', "internal");
+   } else {
+     ga('set', 'dimension1', "external");
+   }
+
+   ga('send', 'pageview', 'ipAddress');
+
+   if(fromInternal) {
+     if(!(clientIP.indexOf("90.216.134.") > -1 || clientIP.indexOf("90.216.150.") > -1)) {
+       window.location = "/"
+     }
+   } else {
+     if(clientIP.indexOf("90.216.134.") > -1 || clientIP.indexOf("90.216.150.") > -1) {
+       window.location = "/internalHtml"
+     }
+   }
+}
